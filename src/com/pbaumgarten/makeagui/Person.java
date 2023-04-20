ee85a2f0a37ee689d7b167914ed7573d17cfada0
justinelecoeur <justine.l2024@isturin.it>
2023-04-20 13:04:45 +0200

    20/4

--- src/com/pbaumgarten/makeagui/Person.java
+++ src/com/pbaumgarten/makeagui/Person.java
@@ -9,20 +9,22 @@ public class Person {
     private String name;
     private String email;
     private String phoneNumber;
-    //private String address;
+    private String address;
     private LocalDate dateOfBirth;
 
-    public Person(String name, String email, String phoneNumber, LocalDate dateOfBirth) {
+    public Person(String name, String email, String phoneNumber, String address, LocalDate dateOfBirth) {
         this.name = name;
         this.email = email;
         this.phoneNumber = phoneNumber;
+        this.address = address;
         this.dateOfBirth = dateOfBirth;
     }
 
-    public Person(String name, String email, String phoneNumber, String dateOfBirth) {
+    public Person(String name, String email, String phoneNumber, String address, String dateOfBirth) {
         this.name = name;
         this.email = email;
         this.phoneNumber = phoneNumber;
+        this.address = address;
         this.setDateOfBirth(dateOfBirth);
     }
 
@@ -45,23 +47,24 @@ public class Person {
     public String getPhoneNumber() {
         return phoneNumber;
     }
-
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
-
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
     public LocalDate getDateOfBirth() {
         return dateOfBirth;
     }
-
     public void setDateOfBirth(LocalDate dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
-
     public void setDateOfBirth(String dateOfBirth) {
         this.dateOfBirth = LocalDate.parse(dateOfBirth, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
     }
-
     public int getAge() {
         LocalDate today = LocalDate.now();
         Period period = Period.between(this.dateOfBirth, today);
--- src/com/pbaumgarten/makeagui/Screen.java
+++ src/com/pbaumgarten/makeagui/Screen.java
@@ -1,11 +1,15 @@
 package com.pbaumgarten.makeagui;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.time.LocalDate;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
@@ -23,6 +27,9 @@ public class Screen extends JFrame {
     private JTextField textDateOfBirth;
     private JPanel panelMain;
     private JList listPeople;
+    private JButton buttonClear;
+    private JButton buttonDelete;
+    private JPanel Image;
     private ArrayList<Person> people;
     private  DefaultListModel ListPeopleModel;
 
@@ -31,10 +38,25 @@ public class Screen extends JFrame {
         this.setContentPane(this.panelMain);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.pack();
+        this.setSize(700, 550);
         people = new ArrayList<Person>();
         ListPeopleModel = new DefaultListModel();
         listPeople.setModel(ListPeopleModel);
         buttonSave.setEnabled(false);
+        buttonClear.setEnabled(false);
+        buttonDelete.setEnabled(false);
+
+        buttonClear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textName.setText("");
+                textEmail.setText("");
+                textPhoneNumber.setText("");
+                textAddress.setText("");
+                textDateOfBirth.setText("");
+                labelAge.setText("");
+            }
+        });
 
         buttonNew.addActionListener(new ActionListener() {
             @Override
@@ -43,6 +65,7 @@ public class Screen extends JFrame {
                         textName.getText(),
                         textEmail.getText(),
                         textPhoneNumber.getText(),
+                        textAddress.getText(),
                         textDateOfBirth.getText()
                 );
                 people.add(p);
@@ -60,10 +83,22 @@ public class Screen extends JFrame {
                     p.setName(textName.getText());
                     p.setEmail(textEmail.getText());
                     p.setPhoneNumber(textPhoneNumber.getText());
+                    p.setAddress(textAddress.getText());
                     if (empty.equals(textDateOfBirth.getText()))
                         p.setDateOfBirth("01/01/2000");
                     else
                         p.setDateOfBirth(textDateOfBirth.getText());
+                    refreshPeopleList();
+                }
+            }
+        });
+
+        buttonDelete.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int personNumber = listPeople.getSelectedIndex();
+                if (personNumber >= 0) {
+                    people.remove(personNumber);
                     refreshPeopleList();
                 }
             }
@@ -78,9 +113,12 @@ public class Screen extends JFrame {
                     textName.setText(p.getName());
                     textEmail.setText(p.getEmail());
                     textPhoneNumber.setText(p.getPhoneNumber());
+                    textAddress.setText(p.getAddress());
                     textDateOfBirth.setText(p.getDateOfBirth().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
                     labelAge.setText(Integer.toString(p.getAge()) + " years");
                     buttonSave.setEnabled(true);
+                    buttonClear.setEnabled(true);
+                    buttonDelete.setEnabled(true);
                 } else
                     buttonSave.setEnabled(false);
             }
@@ -101,18 +139,18 @@ public class Screen extends JFrame {
         refreshPeopleList();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Screen screen = new Screen();
-        screen.setVisible(true);
 
-        Person sheldon = new Person("Sheldon Lee Cooper", "sheldon@gmail.com", "555 0001", "26/02/1980");
-        Person howard = new Person("Howard Joel Wolowitz", "howard@gmail.com", "555 0002", "01/03/1981");
-        Person bernadette = new Person("Bernadette Rostenkowski-Wolowitz", "bernadette@gmail.com", "555 0002",
+
+        Person sheldon = new Person("Sheldon Lee Cooper", "sheldon@gmail.com", "555 0001", "house", "26/02/1980");
+        Person howard = new Person("Howard Joel Wolowitz", "howard@gmail.com", "555 0002", "apartment", "01/03/1981");
+        Person bernadette = new Person("Bernadette Rostenkowski-Wolowitz", "bernadette@gmail.com", "555 0002", "villa", 
                 "01/01/1984");
-        Person raj = new Person("Rajesh Ramayan Koothrappali", "raj@gmail.com", "555 0003", "06/10/1981");
-        Person penny = new Person("Penny Hofstadter", "penny@gmail.com", "555 0004", "02/12/1985");
-        Person leonard = new Person("Leonard Hofstadter", "leonard@gmail.com", "555 0004", "17/05/1980");
-        Person amy = new Person("Amy Farrah Fowler", "amy@gmail.com", "555 0005", "17/12/1979");
+        Person raj = new Person("Rajesh Ramayan Koothrappali", "raj@gmail.com", "555 0003", "city", "06/10/1981");
+        Person penny = new Person("Penny Hofstadter", "penny@gmail.com", "555 0004", "street", "02/12/1985");
+        Person leonard = new Person("Leonard Hofstadter", "leonard@gmail.com", "555 0004", "box", "17/05/1980");
+        Person amy = new Person("Amy Farrah Fowler", "amy@gmail.com", "555 0005", "mansion", "17/12/1979");
 
         screen.addPerson(sheldon);
         screen.addPerson(howard);
@@ -121,6 +159,15 @@ public class Screen extends JFrame {
         screen.addPerson(penny);
         screen.addPerson(leonard);
         screen.addPerson(amy);
+
+        screen.Image.setLayout(new FlowLayout());
+        BufferedImage myImage = ImageIO.read(new File("pic.png"));
+        Image dmyImage = myImage.getScaledInstance(212, 124, java.awt.Image.SCALE_SMOOTH);
+        JLabel picLabel = new JLabel(new ImageIcon(dmyImage));
+        screen.Image.add(picLabel);
+
+        screen.setVisible(true);
+
 
 
     }
